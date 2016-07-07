8180bcd5d10227c454fe808ccfe711f4f2b2c965
Lewis Gavin <gavlaaaaa5@gmail.com>
2016-07-07 13:23:08 +0100

    Arrows and stuff

--- rankedlist.js
+++ rankedlist.js
@@ -11,9 +11,11 @@ function initialTableLoad(tableId){
 }
 
 function updateTable(dataSet, tableId){
+	// Variable used for iterating the column headers
+	//var columns = ["Item", "Count"];
 
 	d3.select('#' + tableId + 'table').selectAll("tbody").html("");
-	d3.select('#' + tableId + 'table').append("tbody").selectAll("tr")
+	/*d3.select('#' + tableId + 'table').append("tbody").selectAll("tr")
 			.data(dataSet).enter()
 			.append("tr")
 			.selectAll("td")
@@ -22,17 +24,39 @@ function updateTable(dataSet, tableId){
 			}).enter()
 			.append('td')
 			.text(function(d) {
-				console.log(d);
 				return d;
-			});
+			});*/
 
 
 
 	/* Set all the cells in columns with THEHEADING in the heading to red */
-	columnTh = $("table th:contains('Item')"); // Find the heading with the text THEHEADING
+	/*columnTh = $("table th:contains('Item')"); // Find the heading with the text THEHEADING
 	columnIndex = columnTh.index() + 1; // Get the index & increment by 1 to match nth-child indexing
-	$('table tr td:nth-child(' + columnIndex + ')').css("color", "#F00"); // Set all the elements with that index in a tr red
+	$('table tr td:nth-child(' + columnIndex + ')').css("color", "#F00"); // Set all the elements with that index in a tr red*/
 
+	var tbody = d3.select('#' + tableId + 'table').append("tbody")
+	dataSet.forEach(function(row) {
+		var tr = tbody.append("tr");
 
+		// If you wanted to define a column variable and iterate through the column headers
+		/*columns.forEach(function(column) {
+			tr.append('td').text(row[column]);
+		})*/
+
+		// Just to show that you can add stylings to each text still
+		//tr.append('td').text(row["Item"] + "\u25B2").style('color', '#F00');
+		
+		tr.append('td').text(row["Item"])
+		.append("span")
+		.attr("class", "pull-right")
+		.text(function(){ 
+			var sentiment = "";
+			if (row["Sentiment"] == "g") sentiment = "\u25B2";
+			else if (row["Sentiment"] == "n") sentiment = "-";
+			else if (row["Sentiment"] == "b") sentiment = "\u25BC";
+			return sentiment;
+		});
+		tr.append('td').text(row["Count"]);
+	})
 }
 
